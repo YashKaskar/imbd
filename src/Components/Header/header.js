68c65afb8bd653c5b96f7da1e60195222514f776
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-07 16:00:09 +0530

    style

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -7,9 +7,9 @@ const Header = () => {
     <div className='header'> 
       <div className="header-left">
         < Link to='/' ><img className='header_icon' alt='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png' /></Link>
-        < Link to= '/movies/popular' >Popular</Link>
-        < Link to= '/movies/top_rated' >Top Rated</Link>
-        < Link to= '/movies/upcoming' >Upcoming</Link>
+        < Link to='/movies/popular' style={{ textDecoration: 'none' }}> <span>Popular</span></Link>
+        < Link to= '/movies/top_rated' style={{ textDecoration: 'none' }} ><span>Top Rated</span></Link>
+        < Link to= '/movies/upcoming' style={{ textDecoration: 'none' }} ><span>Upcoming</span></Link>
       </div>
     </div>
   )
