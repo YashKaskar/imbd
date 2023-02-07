fed6f43802b8cab6b24a952bf12ba2ecb1b1b602
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-07 15:49:24 +0530

    alt in img

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/header';
 
 function App() {
   return (
     <div className="App">
       <Router> 
+        < Header />
+
         <Routes>
           <Route index element={<h1>Hello World</h1>}></Route>
           <Route path='movie/:id' element={<h1>Movie details page</h1>}></Route>
           <Route path='movies/:type' element={<h1>Movie list page</h1>}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
         </Routes>
+        
      </Router>
     </div>
   );
