85dfbaade8381b7ba5a4fcd952d8b1e2a129102e
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-07 15:49:39 +0530

    alt

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return(
     <div className='header'> 
       <div className="header-left">
-        < Link to='/' ><img className='header_icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png' /></Link>
+        < Link to='/' ><img className='header_icon' alt='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png' /></Link>
         < Link to= '/movies/popular' >Popular</Link>
         < Link to= '/movies/top_rated' >Top Rated</Link>
         < Link to= '/movies/upcoming' >Upcoming</Link>
