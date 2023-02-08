da20794fc4cde4477c085186af7394b222ffacd3
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-08 14:39:15 +0530

    react resposive carousel css and import carousel

--- src/SitePage/Home/home.js
+++ src/SitePage/Home/home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './home.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Home = () => {
 
