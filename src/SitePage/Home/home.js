e7e91bbd226b64fadfd3dab271dc158d55fbefe1
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-08 15:26:22 +0530

    mapping the popular movies

--- src/SitePage/Home/home.js
+++ src/SitePage/Home/home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './home.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -18,11 +19,40 @@ const Home = () => {
 
 
   return (
-    <div>
-          <>
-          Home Page   
-          </>
-    </div>
+    <>
+      <div className="poster">
+        < Carousel
+        
+          showThumbs = {false}
+          autoPlay = {true}
+          transitionTime = {3}
+          infiniteLoop = {true}
+          showStatus = {false}
+        >
+          {
+            popularMovies.map(movie => ( 
+            < Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`}>
+              <div className="posterImage">
+                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+              </div>
+              <div className="posterImage__overlay">
+               <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
+               <div className="posterImage__runtime">
+                {movie ? movie.release_date : ""}
+                 <span className="posterImage__rating">
+                  {movie ? movie.vote_average :""}
+                  <i className="fas fa-star" />{" "}
+                 </span>
+               </div>
+               <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+              </div>
+            </Link>
+            ))
+          }
+          
+        </Carousel>
+      </div> 
+    </>
   )
 }
 
