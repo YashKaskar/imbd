b829599b86c51643d12f643ff8d1a108b8676ce0
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-10 15:40:49 +0530

    movie details page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/header';
 import Home from './SitePage/Home/home';
 import MovieList from './Components/moiveList/movieList';
+import Movie from './SitePage/movieDetails/movieDetails';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
 
         <Routes>
           <Route index element={< Home />}></Route>
-          <Route path='movie/:id' element={<h1>Movie details page</h1>}></Route>
+          <Route path='movie/:id' element={< Movie />}></Route>
           <Route path='movies/:type' element={< MovieList />}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
         </Routes>
--- src/SitePage/Home/home.js
+++ src/SitePage/Home/home.js
@@ -3,6 +3,7 @@ import './home.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from 'react-router-dom';
+import MovieList from './../../Components/moiveList/movieList';
 
 const Home = () => {
 
@@ -51,6 +52,7 @@ const Home = () => {
           }
           
         </Carousel>
+        < MovieList />
       </div> 
     </>
   )
