2e3a4fab90c799f5f1238fa8aa05401e216f8073
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-10 14:29:05 +0530

    adding movie List route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/header';
 import Home from './SitePage/Home/home';
+import MovieList from './Components/moiveList/movieList';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route index element={< Home />}></Route>
           <Route path='movie/:id' element={<h1>Movie details page</h1>}></Route>
-          <Route path='movies/:type' element={<h1>Movie list page</h1>}></Route>
+          <Route path='movies/:type' element={< MovieList />}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
         </Routes>
         
