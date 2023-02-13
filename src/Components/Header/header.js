2ad3103ca7f59d9a98c05e93b8840aa44e7d5c56
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-13 09:54:01 +0530

    avatar

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -9,7 +9,12 @@ const Header = () => {
         < Link to='/' ><img className='header_icon' alt='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png' /></Link>
         < Link to='/movies/popular' style={{ textDecoration: 'none' }}> <span>Popular</span></Link>
         < Link to= '/movies/top_rated' style={{ textDecoration: 'none' }} ><span>Top Rated</span></Link>
-        < Link to= '/movies/upcoming' style={{ textDecoration: 'none' }} ><span>Upcoming</span></Link>
+        < Link to='/movies/upcoming' style={{ textDecoration: 'none' }} ><span>Upcoming</span></Link>
+      </div>
+      <div className="header-right">
+        <span className='user_avatar'>  
+        <i className="fas fa-user"/>
+        </span>
       </div>
     </div>
   )
