a3627ae2e0794f1f63eff3e4fcaa07875536965c
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-15 15:55:05 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
