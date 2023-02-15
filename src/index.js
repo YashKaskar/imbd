b59b9643828129ae2afceb458f867a55ba715e98
Yash Kaskar <yash.kaskar99@gmail.com>
2023-02-15 15:54:31 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Router } from 'react-router-dom';
 
 
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, 
-document.getElementById('root')
+ReactDOM.render(document.getElementById('root'));
+root.render(
+    <App />
 );
-
-    
 
